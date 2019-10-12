4fbb949ac77b54ed681b561ac75bd6a0e894caf3
YulianSalo <julian_salo@yahoo.com>
2019-10-12 17:06:31 +0300

    Google drive script backup

---
+++ gdrive.sh
@@ -0,0 +1,14 @@
+cd ~
+wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
+
+
+mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
+
+ 	
+chmod +x gdrive
+
+
+sudo install gdrive /usr/local/bin/gdrive
+
+
+gdrive list
